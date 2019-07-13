911ad677f5a76fcb1be4f5c8c7c857b959a26819
sdreyes <sheshechan90@gmail.com>
2019-07-13 13:14:56 -0400

    Display a map of Philly with three random markers and console log the response from the FourSquare API call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,43 @@
 var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 39.95233, lng: -75.16379 },
-    zoom: 13
-  });
+
+  map = new google.maps.Map(
+    document.getElementById('map'),
+    { center: new google.maps.LatLng(39.952583, -75.165222), zoom: 16 });
+
+  var features = [
+    {
+      position: new google.maps.LatLng(39.952583, -75.165222),
+    }, {
+      position: new google.maps.LatLng(39.952583, -75.165222),
+    }, {
+      position: new google.maps.LatLng(39.952583, -75.165222),
+    }
+  ];
+
+  for (var i = 0; i < features.length; i++) {
+    var marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: "https://www.google.com/mapfiles/arrow.png",
+      map: map
+    });
+  };
+
+  $(document).on("click", "body", function() {
+    // var marker = new google.maps.Marker({
+    //   position: new google.maps.LatLng(-33.914139, 151.240704),
+    //   icon: icons["test"].icon,
+    //   map: map
+    // });
+    // console.log("hi");
+  })
+
 }
+
+$.ajax({
+  url: "https://api.foursquare.com/v2/venues/search?client_id=2WOZVF2YN3B42HG13D21JEOHUSK5PMAV2JTSWOXHDRUK1GGW&client_secret=HSO1YHS2JPH1HAKU31TIAPIWPDD21CHKUUEEGT2RUYOFESYY&ll=39.952583,-75.165222&categoriId=4deefb944765f83613cdba6e&v=20190713",
+  method: "GET"
+}).then(function (response) {
+  console.log(response.response.venues);
+})
